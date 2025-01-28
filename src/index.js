baac30b05a70dec72c3fbd09141cfcfc7f0e9f80
Daisy Jimenez <jimenez.daisy9494@gmail.com>
2025-01-28 15:07:46 -0800

    tasklister project

--- src/index.js
+++ src/index.js
@@ -1,28 +1,27 @@
 document.addEventListener("DOMContentLoaded", () => {
-  const taskInput = document.getElementById("new-task-description");
-  const taskForm = document.getElementById("create-task-form");
-  const taskList = document.getElementById("tasks");
+  const taskList = new TaskList();
+
+  const elementIds = ["create-task-form", "new-task-description", "new-task-priority", "tasks"];
+  const [taskForm, taskInput, taskPriority, taskUl] = elementIds.map(id => document.getElementById(id));
 
   const addTask = () => {
-      const task = taskInput.value.trim();
-      if (task) {
-          const li = document.createElement("li");
-          li.textContent = task;
+    const task = taskInput.value.trim();
+    if (task) {
+      const li = document.createElement("li");
+      li.textContent = task;
 
-          const deleteButton = document.createElement("button");
-          deleteButton.textContent = "x";
-          deleteButton.addEventListener("click", () => li.remove());
+      const deleteButton = document.createElement("button");
+      deleteButton.textContent = "x";
+      deleteButton.addEventListener("click", () => li.remove());
 
-          li.appendChild(deleteButton);
-          taskList.appendChild(li);
-          taskInput.value = "";
-      }
+      li.appendChild(deleteButton);
+      taskUl.appendChild(li);
+      taskInput.value = "";
+    }
   };
 
   taskForm.addEventListener("submit", (e) => {
-      e.preventDefault();
-      addTask();
+    e.preventDefault();
+    addTask();
   });
 });
-
-  
